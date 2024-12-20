3aa69bd64c646d25d15e9eac69cabdd6a55664e5
AhmedNasser1010 <ahmed.mix.games@gmail.com>
2024-12-20 23:26:10 +0200

    add example data

---
+++ src/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO trains (train_number, train_type, departure_station, arrival_station)
+VALUES ('123', 'Express', 'Cairo', 'Alexandria');
+
+INSERT INTO stations (station_name) VALUES ('Cairo'), ('Giza'), ('Tanta'), ('Alexandria');
